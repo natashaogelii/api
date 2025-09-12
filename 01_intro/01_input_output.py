c48df5eba3641df8fd7d4b8a3fef2fbb3897ad27
kelvinokumu <oumakelvinokumu@gmail.com>
2025-09-12 12:03:43 +0300

    intro

---
+++ 01_intro/01_input_output.py
@@ -0,0 +1,14 @@
+name = input("Enter a name ")
+# num  = input("Enter a number ") # type casting
+num  = int(input("Enter a number "))
+
+print(f"The name is {name} and the number is {num}")
+
+
+# type cast
+print("The name is " +name+ " and the number is " +str(num))
+
+# c way 
+
+
+# .format
---
+++ 01_intro/02_python_types.py
@@ -0,0 +1,11 @@
+num = 123
+print(type(num))
+
+name = "Damian"
+print(type(name))
+
+flag = True
+print(type(flag))
+
+num3 = 123.25
+print(type(num3))
